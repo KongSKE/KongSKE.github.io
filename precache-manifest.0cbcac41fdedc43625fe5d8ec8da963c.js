478f1050fedc01f4226a6b1bb9d6f73ae8fd6b43
varito <varit.as@ku.th>
2019-02-15 13:28:49 +0700

    Updates

---
+++ precache-manifest.0cbcac41fdedc43625fe5d8ec8da963c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "f19a3c679afbb1617dbf",
+    "url": "/static/js/main.f19a3c67.chunk.js"
+  },
+  {
+    "revision": "ba1ca5ba3f9c1e3c91ad",
+    "url": "/static/js/2.ba1ca5ba.chunk.js"
+  },
+  {
+    "revision": "f19a3c679afbb1617dbf",
+    "url": "/static/css/main.f9387cd4.chunk.css"
+  },
+  {
+    "revision": "38539a2efb9a370a53bb77a9ba33c748",
+    "url": "/index.html"
+  }
+];
